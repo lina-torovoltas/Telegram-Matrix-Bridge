ba1cd8d34441f99d833f09d1ba9af45e502b1f90
Lina Torovoltas <193523522+lina-torovoltas@users.noreply.github.com>
2025-02-18 08:22:21 +0300

    Restart added

--- start_bridge.sh
+++ start_bridge.sh
@@ -3,24 +3,44 @@
 # Determine the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Create the first tmux session for Telegram bot
-tmux new-session -d -s telegram_bot -c "$SCRIPT_DIR"
+# function for launching a Telegram bot in tmux with auto-restart
+start_tg_bot() {
+    while true; do
+        tmux new-session -d -s tg_session -c "$SCRIPT_DIR"
 
-# Check for the presence of a virtual environment and create it if it doesn’t exist
-if [ ! -d "venv" ]; then
-    tmux send-keys -t tg_session 'python -m venv venv' C-m
-fi
-tmux send-keys -t tg_session 'source venv/bin/activate' C-m
-tmux send-keys -t tg_session "pip install -r requirements.txt" C-m
-tmux send-keys -t tg_session 'python Bridge/tgbot.py' C-m
+        # Checks for the existence of a virtual environment and creates it if it doesn't exist
+        if [ ! -d "venv" ]; then
+            tmux send-keys -t tg_session 'python -m venv venv' C-m
+        fi
+        tmux send-keys -t tg_session 'source venv/bin/activate' C-m
+        tmux send-keys -t tg_session "pip install -r requirements.txt" C-m
+        tmux send-keys -t tg_session 'python Bridge/tgbot.py' C-m
+        
+        # Waits an hour before restarting
+        sleep 3600
+        tmux kill-session -t tg_session
+    done
+}
 
-# Create a second tmux session for Matrix bot
-tmux new-session -d -s matrix_bot -c "$SCRIPT_DIR"
+# Function for launching Matrix bot in tmux with auto-restart
+start_mt_bot() {
+    while true; do
+        tmux new-session -d -s mt_session -c "$SCRIPT_DIR"
 
-# Check for the presence of a virtual environment and create it if it doesn’t exist
-if [ ! -d "venv" ]; then
-    tmux send-keys -t mt_session 'python -m venv venv' C-m
-fi
-tmux send-keys -t mt_session 'source venv/bin/activate' C-m
-tmux send-keys -t mt_session "pip install -r requirements.txt" C-m
-tmux send-keys -t mt_session 'python Bridge/mtbot.py' C-m
+	# Checks for the existence of a virtual environment and creates it if it doesn't exist
+        if [ ! -d "venv" ]; then
+            tmux send-keys -t mt_session 'python -m venv venv' C-m
+        fi
+        tmux send-keys -t mt_session 'source venv/bin/activate' C-m
+        tmux send-keys -t mt_session "pip install -r requirements.txt" C-m
+        tmux send-keys -t mt_session 'python Bridge/mtbot.py' C-m
+        
+        # Waits an hour before restarting
+        sleep 3600
+        tmux kill-session -t mt_session
+    done
+}
+
+# Starts both bots
+start_tg_bot &
+start_mt_bot &
